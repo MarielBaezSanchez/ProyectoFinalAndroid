951511a75c19d4b138fe8d960141612345694675
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-12-12 17:49:25 -0600

    Video diez terminado

--- app/src/main/java/com/example/instagram/ui/theme/Type.kt
+++ app/src/main/java/com/example/instagram/ui/theme/Type.kt
@@ -11,21 +11,19 @@ import com.example.instagram.R
 
 val instagramFont = FontFamily(Font(R.font.billabong))
 
-fun typography(onBackground:Color): Typography{
+fun typography(onBackGround:Color): Typography{
     return Typography(
         bodyLarge = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
             fontSize = fontSmall,
-            lineHeight = 24.sp,
-            letterSpacing = 0.5.sp,
-            color = onBackground
+            color = onBackGround
         ),
         headlineLarge = TextStyle(
             fontFamily = instagramFont,
             fontWeight = FontWeight.Normal,
             fontSize = fontXXLarge,
-            color = onBackground
+            color = onBackGround
         )
     )
 }
