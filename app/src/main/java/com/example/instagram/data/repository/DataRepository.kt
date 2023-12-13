d81b159ac6d4fcdba84ddd41d274bcd114652958
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2023-12-12 18:55:41 -0600

    Video once terminado

--- app/src/main/java/com/example/instagram/data/repository/DataRepository.kt
+++ app/src/main/java/com/example/instagram/data/repository/DataRepository.kt
@@ -1,18 +1,294 @@
 package com.example.instagram.data.repository
 
+import com.example.instagram.data.model.Feed
 import com.example.instagram.data.model.Story
 
 val stories = listOf(
-    Story(userNickName = "marielbaez", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "samaelbaez", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "arochU", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "sahuSA", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "unaHada", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "ilakechSBS", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "cecySA", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "robot", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "yeye", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "yeyo", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "mikaVS", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
-    Story(userNickName = "Salazar", userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"),
+    Story(
+        userNickName = "alexturner",
+        userAvatar = "https://collectionimages.npg.org.uk/large/mw263083/Alex-Turner.jpg"
+    ),
+    Story(
+        userNickName = "mggs",
+        userAvatar = "https://akamai.sscdn.co/tb/letras-blog/wp-content/uploads/2022/07/4930a5e-Joji-1024x741.jpg"
+    ),
+    Story(
+        userNickName = "SamIlakech",
+        userAvatar = "https://i.pinimg.com/736x/98/c8/80/98c88087cf3e0db1db4f67eaa9755ff9.jpg"
+    ),
+    Story(
+        userNickName = "tostao",
+        userAvatar = "https://static-img.zz.pt/history/imgS620I11658T20190122154103.png"
+    ),
+    Story(
+        userNickName = "barbaraliskov",
+        userAvatar = "https://www.ufrgs.br/enigma/wp-content/uploads/2021/11/20091222102310-0_0.jpg"
+    ),
+    Story(
+        userNickName = "addhanari",
+        userAvatar = "https://aventurasnahistoria.uol.com.br/media/uploads/personagem/cazuza_capa.jpg"
+    ),
+    Story(
+        userNickName = "luisito",
+        userAvatar = "https://i.scdn.co/image/ab67706c0000da84196218f1a3b36ca7d57d890d"
+    ),
+    Story(
+        userNickName = "jovemwerther",
+        userAvatar = "https://casadoestudo.com/wp-content/uploads/2023/01/Goethe.webp"
+    ),
+    Story(
+        userNickName = "cruzeiro",
+        userAvatar = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Cruzeiro_Esporte_Clube_%28logo%29.svg/1200px-Cruzeiro_Esporte_Clube_%28logo%29.svg.png"
+    ),
+    Story(
+        userNickName = "winston",
+        userAvatar = "https://www.revistabula.com/wp/wp-content/uploads/2020/08/george_orwell.jpg"
+    ),
+    Story(
+        userNickName = "neguinism",
+        userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNKha0-2mYtaU1fklbUA_aSDGYM4BMT--aeXYRi74vgHDSkuylwbkWpeGsYPofU1k3Ci8&usqp=CAU"
+    ),
+    Story(
+        userNickName = "sloane",
+        userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg"
+    ),
+    Story(
+        userNickName = "xr320i",
+        userAvatar = "https://farm5.static.flickr.com/4071/4210989776_280d7bd40c.jpg"
+    ),
+    Story(
+        userNickName = "capitu",
+        userAvatar = "https://s01.video.glbimg.com/x240/2882588.jpg",
+    ),
+    Story(
+        userNickName = "kotlin",
+        userAvatar = "https://miro.medium.com/v2/resize:fit:1400/1*MCU8J5-roudxIIYt1U1J1A.jpeg"
+    ),
+    Story(
+        userNickName = "bluepen",
+        userAvatar = "https://amazonasatual.com.br/wp-content/uploads/2019/11/manoel-gomes.jpg"
+    ),
+    Story(
+        userNickName = "dumont",
+        userAvatar = "https://jpimg.com.br/uploads/2019/09/santos-dumont.jpg"
+    ),
+    Story(
+        userNickName = "noel",
+        userAvatar = "https://www.mapadoceu.com.br/photo/astro/noel-rosa.jpg"
+    ),
+    Story(
+        userNickName = "renato",
+        userAvatar = "https://uploads.metropoles.com/wp-content/uploads/2019/04/11191551/DSC7795a.jpeg"
+    ),
+    Story(
+        userNickName = "deassis",
+        userAvatar = "https://cdn.brasildefato.com.br/media/d3e65c869db03180a7d34b1569ccdc04.jpg"
+    ),
+    Story(
+        userNickName = "tricks&combo",
+        userAvatar = "https://i.ytimg.com/vi/UNPuyZwrwXw/mqdefault.jpg"
+    ),
+    Story(
+        userNickName = "lilisaac",
+        userAvatar = "https://www.infoescola.com/wp-content/uploads/2008/09/isaac-newton-328x450.jpg"
+    ),
+    Story(
+        userNickName = "margarethamilton",
+        userAvatar = "https://afala.com.br/wp-content/uploads/Margaret_Hamilton_in_action.0.0-e1544014483854.jpg"
+    )
+)
+
+val feedList = listOf(
+    Feed(
+        userNickname = "alexturner",
+        userAvatar = "https://collectionimages.npg.org.uk/large/mw263083/Alex-Turner.jpg",
+        localName = "Sheffield - UK",
+        imageUrl = "https://www.rbsdirect.com.br/imagesrc/15217916.jpg?w=700",
+        description = "I wanna be your Ford Cortina I will never rust...",
+        postedAgo = "Hace 21 horas"
+    ),
+    Feed(
+        userNickname = "mggs",
+        userAvatar = "https://akamai.sscdn.co/tb/letras-blog/wp-content/uploads/2022/07/4930a5e-Joji-1024x741.jpg",
+        localName = "Osaka - Japão",
+        imageUrl = "https://i.ytimg.com/vi/FvOpPeKSf_4/maxresdefault.jpg",
+        description = "Perfect don't mean that it's working. So what can I do? Cause sometimes I look in her eyes and that's where I find a glimpse of us. And I try to fall for her touch. But I'm thinkin' of the way it was said I'm fine and said I moved on. I'm only here passin' time in her arms, hopin' I find a glimpse of us.",
+        postedAgo = "Hace 2 dias"
+    ),
+    Feed(
+        userNickname = "tricks&combo",
+        userAvatar = "https://i.ytimg.com/vi/UNPuyZwrwXw/mqdefault.jpg",
+        localName = "Rival vs Rival",
+        imageUrl = "https://www.deltager.no/img/6672/93308H3CL2D6J.jpg",
+        description = "Alan - Biohazard",
+        postedAgo = "Hace 4 dias"
+    ),
+    Feed(
+        userNickname = "SamIlakech",
+        userAvatar = "https://i.pinimg.com/736x/98/c8/80/98c88087cf3e0db1db4f67eaa9755ff9.jpg",
+        localName = "Localiza ai",
+        imageUrl = "https://i0.wp.com/www.autocustom.com.br/wp-content/uploads/2017/05/a7ab71535e6118088d874a5ad20fcbb5.jpg?fit=1200%2C795&ssl=1",
+        description = "Hay que creer siempre que el sueño es posible, que el cielo es el límite y vosotros, truchas, sois imbatibles. Que los malos momentos pasarán, es solo una fase en la que el sufrimiento alimenta tu coraje más que tu familia te necesita, codo con codo si ganas para apoyarte si pierdes.",
+        postedAgo = "Hace 10 horas"
+    ),
+    Feed(
+        userNickname = "barbaraliskov",
+        userAvatar = "https://www.ufrgs.br/enigma/wp-content/uploads/2021/11/20091222102310-0_0.jpg",
+        localName = "Los Angeles - CA",
+        imageUrl = "https://stackify.com/wp-content/uploads/2018/04/SOLID-Principles-Liskov-Substitution-1-881x441-1.png",
+        description = "Principio de inversión de Liskov: una guía fundamental en programación orientada a objetos. Establece que las clases derivadas deben ser reemplazables por sus clases base, sin cambiar el comportamiento esperado del programa. En otras palabras, si el código funciona con una clase determinada, también debe funcionar con sus subclases. Esto asegura la coherencia y extensibilidad del sistema, evitando sorpresas no deseadas. Mantenga sus clases en armonía y cree código sólido con Liskov Inversion.",
+        postedAgo = "Hace 22 horas"
+    ),
+    Feed(
+        userNickname = "tostao",
+        userAvatar = "https://static-img.zz.pt/history/imgS620I11658T20190122154103.png",
+        localName = "Belo Horizonte - MG",
+        imageUrl = "https://i.superesportes.com.br/wEPWY4M5T4JArv6VobXImoqp1-Y=/650x0/smart/imgsapp.mg.superesportes.com.br/app/noticia_126420360808/2022/12/31/3984997/dirceu-lopes-pele-e-tostao-reunidos-em-jogo-entre-cruzeiro-e-santos_1_360335.jpg",
+        description = "¡Gran dia!",
+        postedAgo = "Hace 21 horas"
+    ),
+    Feed(
+        userNickname = "addhanari",
+        userAvatar = "https://aventurasnahistoria.uol.com.br/media/uploads/personagem/cazuza_capa.jpg",
+        localName = "Rio de Janeiro - RJ",
+        imageUrl = "https://img.youtube.com/vi/ZBwjT-3t2O8/0.jpg",
+        description = "EXAGERADO… Arrojado a tus pies, soy realmente exagerado. ¡Me encanta el amor inventado!",
+        postedAgo = "Hace 21 horas"
+    ),
+    Feed(
+        userNickname = "jovemwerther",
+        userAvatar = "https://casadoestudo.com/wp-content/uploads/2023/01/Goethe.webp",
+        localName = "Frankfurt - Alemanha",
+        imageUrl = "https://brainly.com.br/_next/image?url=https%3A%2F%2Fpt-static.z-dn.net%2Ffiles%2Fd46%2F3c7459b8d9b596496031e2a527bf4967.jpg&w=1080&q=75",
+        description = "Es cierto, amigo mío, que cada día comprendo mejor lo absurdo que es vivir juzgando a los demás. Por mi parte, tengo tanto que hacer para cambiarme, tanto esfuerzo que dedicar para calmar las tormentas de mi corazón!... ¡Ah! Con mucho gusto dejaré que los demás hagan lo que quieran, siempre y cuando me dejen hacer lo mismo.",
+        postedAgo = "Hace 5 dias"
+    ),
+    Feed(
+        userNickname = "churrasco021",
+        userAvatar = "https://i.scdn.co/image/ab67706c0000da84196218f1a3b36ca7d57d890d",
+        localName = "021",
+        imageUrl = "https://portalpopclub.com.br/wp-content/uploads/2023/02/image-29.png",
+        description = "Cuando te encuentro es una secuencia de lovezinho \uD83C\uDFB5",
+        postedAgo = "Hace 5 horas"
+    ),
+    Feed(
+        userNickname = "luisito",
+        userAvatar = "https://logodetimes.com/wp-content/uploads/cruzeiro.png",
+        localName = "Belo Horizonte - MG",
+        imageUrl = "https://i.ytimg.com/vi/-TUNndiobJo/maxresdefault.jpg",
+        description = "Para recordar la emblemática y emotiva narración del gol de la Libertadores del 76, cuando Joãozinho “irresponsablemente” cobró el tiro libre que pertenecía a Nelinho, probablemente el mejor lanzador de faltas del mundo en ese momento. Durante la narración, el narrador Vilibaldo Alves recuerda a nuestro delantero Roberto Batata, fallecido durante la Libertadores de ese año: - “El jugador argentino Ortiz se queja, está delante de la pelota, le pegan mucho, es bueno, es Nelinho. ¡quién tiene que pegarle! Hay que alejarse, tomar posición, meter al extranjero con el balón y todo en la portería, balón… ¡aaaaaaa adivina qué! Joãozinho por el amor de Dios Joãozinho! Tú Joãozinho, Joãozinho... que Roberto Batata desde el cielo lo abrace aquí en la tierra Joãozinho... en este momento recuerdo a Roberto Batata, el que tanto luchó en esta libertadora para Cruzeiro marca el gol que celebra Brasil, ahora esperemos , hinchas de Brasil… ¡Cruzeiro 3, River Plate 2! River quiere pelear, pero Cruzeiro gana en el fútbol”.",
+        postedAgo = "Hace 6 horas"
+    ),
+    Feed(
+        userNickname = "neguinism",
+        userAvatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNKha0-2mYtaU1fklbUA_aSDGYM4BMT--aeXYRi74vgHDSkuylwbkWpeGsYPofU1k3Ci8&usqp=CAU",
+        localName = "Por ai",
+        imageUrl = "https://versatille.com/wp-content/uploads/177640_284533_entre_os_jurados__destaque_para_o_brasileiro_neguin-1.jpg",
+        description = "Dance!",
+        postedAgo = "Hace 7 dias"
+    ),
+    Feed(
+        userNickname = "winston",
+        userAvatar = "https://www.revistabula.com/wp/wp-content/uploads/2020/08/george_orwell.jpg",
+        localName = "Oceania",
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRj1Q3dCBj_-ztP3okvcBcVkSSj7rr9vuVUKQ&usqp=CAU",
+        description = "La libertad es la libertad de decir que dos más dos son cuatro.",
+        postedAgo = "Hace 2 dias"
+    ),
+    Feed(
+        userNickname = "margarethamilton",
+        userAvatar = "https://afala.com.br/wp-content/uploads/Margaret_Hamilton_in_action.0.0-e1544014483854.jpg",
+        localName = "Indiana - EUA",
+        imageUrl = "https://aventurasnahistoria.uol.com.br/media/_versions/personagem/margaret_hamilton_capa_widelg.jpg",
+        description = "En mi época, no existía el autocompletado.",
+        postedAgo = "Hace 9 dias"
+    ),
+    Feed(
+        userNickname = "android",
+        userAvatar = "https://cdn-icons-png.flaticon.com/512/518/518705.png",
+        localName = "Linux",
+        imageUrl = "https://extelos.com/assets/images/blog/jetpack-compose.png",
+        description = "Jetpack Compose es un marco moderno para desarrollar interfaces de usuario en Android. Se introdujo para simplificar y mejorar la creación de aplicaciones de Android al proporcionar un enfoque declarativo y más intuitivo para crear interfaces de usuario. Con Jetpack Compose, los desarrolladores pueden crear interfaces dinámicas y receptivas utilizando Kotlin, aprovechando el poder de la programación funcional y la reactividad.",
+        postedAgo = "Hace 6 minutos"
+    ),
+    Feed(
+        userNickname = "xr320i",
+        userAvatar = "https://farm5.static.flickr.com/4071/4210989776_280d7bd40c.jpg",
+        localName = "1993",
+        imageUrl = "https://revistafullpower.com.br/wp-content/uploads/2013/04/meu-carro-na-FULL-4.jpg",
+        description = "MK5",
+        postedAgo = "Hace 3 dias"
+    ),
+    Feed(
+        userNickname = "capitu",
+        userAvatar = "https://s01.video.glbimg.com/x240/2882588.jpg",
+        localName = "Tiradentes - MG",
+        imageUrl = "https://www1.folha.uol.com.br/folha/galeria/album/images/20081208-capitu1.jpg",
+        description = "No soy ni bueno ni malo, soy la persona más complicada que conozco.",
+        postedAgo = "Hace 21 horas"
+    ),
+    Feed(
+        userNickname = "kotlin",
+        userAvatar = "https://miro.medium.com/v2/resize:fit:1400/1*MCU8J5-roudxIIYt1U1J1A.jpeg",
+        localName = "Rússia",
+        imageUrl = "https://blog.logrocket.com/wp-content/uploads/2022/04/kotlin-scoping-functions.png",
+        description = "Las funciones de alcance de Kotlin son un conjunto de funciones que le permiten ejecutar un bloque de código en un objeto con una sintaxis concisa. Cada función tiene una forma de hacer referencia al objeto de contexto y un valor de retorno específico. Hay cinco: dejar, ejecutar, con, aplicar y también.",
+        postedAgo = "Hace 5 horas"
+    ),
+    Feed(
+        userNickname = "bluepen",
+        userAvatar = "https://amazonasatual.com.br/wp-content/uploads/2019/11/manoel-gomes.jpg",
+        localName = "Lá Ele - BA",
+        imageUrl = "https://www.bnews.com.br/media/_versions/julho_2023/manoel_gomes_l5dlzkh_widelg_widexl.jpg",
+        description = "Look, if you don't love me, so don't call me don't keep complaining to me.",
+        postedAgo = "Hace 2 dias"
+    ),
+    Feed(
+        userNickname = "dumont",
+        userAvatar = "https://jpimg.com.br/uploads/2019/09/santos-dumont.jpg",
+        localName = "Minas Gerais",
+        imageUrl = "https://oespecialista.com.br/wp-content/uploads/2021/10/santos-dumont-14-bis-voo-518x307.jpg",
+        description = "Es un gran placer compartir con ustedes las apasionantes pruebas realizadas con el 14bis, mi revolucionario invento. A lo largo de estos viajes por los cielos, he sido testigo de la evolución del vuelo y del esfuerzo incansable por alcanzar nuevas alturas. Las pruebas 14bis han sido momentos de desafío y superación, en los que pude presenciar la realización de un viejo sueño: el sueño de ver al hombre conquistando el aire de forma controlada.",
+        postedAgo = "Hace 1 mês"
+    ),
+    Feed(
+        userNickname = "sloane",
+        userAvatar = "https://i.redd.it/rmk3k21tcxu21.jpg",
+        localName = "USA",
+        imageUrl = "https://conteudo.imguol.com.br/c/entretenimento/ea/2016/09/27/cena-do-filme-curtindo-a-vida-adoidado-1986-de-john-hughes-1474995071751_v2_600x450.jpg.webp",
+        description = ":D",
+        postedAgo = "Hace 2 dias"
+    ),
+    Feed(
+        userNickname = "noel",
+        userAvatar = "https://www.mapadoceu.com.br/photo/astro/noel-rosa.jpg",
+        localName = "Vila Isabel",
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoqh35QTclAixwAmaVRyvY2VmIXZB0LUpPan4SJUiUCyOClYK_Y86yhgUlgKivgODVdEw&usqp=CAU",
+        description = "Si un amigo te pide que le digas si me quieres o no. Di que me adoras, que te arrepientes y lloras por nuestra separación. A la gente que odio, diles siempre que no sirvo para nada y que mi casa es el bar. Que arruiné tu vida, que no merezco la comida que pagaste por mí.",
+        postedAgo = "Hace 2 meses"
+    ),
+    Feed(
+        userNickname = "lilisaac",
+        userAvatar = "https://www.infoescola.com/wp-content/uploads/2008/09/isaac-newton-328x450.jpg",
+        localName = "Woolsthorpe Manor - UK",
+        imageUrl = "https://realidadesimulada.com/wp-content/uploads/2018/03/1200_1490707998990_1458417944newton.jpg",
+        description = "¡Eso es realmente un pelo hermoso!",
+        postedAgo = "Hace 8 dias"
+    ),
+    Feed(
+        userNickname = "renato",
+        userAvatar = "https://uploads.metropoles.com/wp-content/uploads/2019/04/11191551/DSC7795a.jpeg",
+        localName = "Brasilia",
+        imageUrl = "https://hqrock.files.wordpress.com/2012/03/legiao_urbana-rampa-do-congresso.jpg",
+        description = "Ojalá, al menos una vez, pudiera recuperar todo el oro que le di a quien logró convencerme de que era prueba de amistad si alguien me quitaba incluso lo que no tenía.",
+        postedAgo = "Hace 2 dias"
+    ),
+    Feed(
+        userNickname = "deassis",
+        userAvatar = "https://cdn.brasildefato.com.br/media/d3e65c869db03180a7d34b1569ccdc04.jpg",
+        localName = "Tiradentes - MG",
+        imageUrl = "https://i.pinimg.com/originals/2b/43/cf/2b43cf682ec5697cedcd6d75df3a7392.png",
+        description = "Ojos gitanos oblicuos disfrazados. No sabía lo que era oblicuo, pero oculto...",
+        postedAgo = "Hace 21 horas"
+    )
 )
--- app/src/main/java/com/example/instagram/ui/view/HomeScreen.kt
+++ app/src/main/java/com/example/instagram/ui/view/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Divider
@@ -14,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.instagram.data.model.Feed
 import com.example.instagram.data.model.Story
+import com.example.instagram.data.repository.feedList
 import com.example.instagram.data.repository.stories
 import com.example.instagram.ui.theme.DividerColor
 import com.example.instagram.ui.theme.spacingMedium
@@ -35,16 +37,7 @@ fun HomeScreen() {
 
         Divider(color = DividerColor, thickness = 0.2.dp)
 
-        FeedItem(
-            feed = Feed(
-                userNickname = "denisse",
-                localName = "San Andres - Tzompantepec",
-                userAvatar = "https://cdn.brasildefato.com.br/media/d3e65c869db03180a7d34b1569ccdc04.jpg",
-                imageUrl = "https://i.pinimg.com/originals/2b/43/cf/2b43cf682ec5697cedcd6d75df3a7392.png",
-                description = "Ojos gitanos oblicuos",
-                postedAgo = "Hace 21 horas"
-            )
-        )
+        FeedList(feedList = feedList)
     }
 }
 
@@ -53,6 +46,15 @@ fun StoryList(stories: List<Story>) {
     LazyRow(modifier = Modifier.padding(top = spacingMedium)) {
         itemsIndexed(stories) { _, item ->
             StoryItem(story = item)
+        }
+    }
+}
+
+@Composable
+fun FeedList(feedList: List<Feed>) {
+    LazyColumn(modifier = Modifier.padding(top = spacingMedium)) {
+        itemsIndexed(feedList) { _, item ->
+            FeedItem(feed = item)
         }
     }
 }
